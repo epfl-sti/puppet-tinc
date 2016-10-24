aae63eff6be71bf57d44807fea4d5714721d197a
iclabsrv admins <iclabsrv-admins@groupes.epfl.ch>
2016-10-24 08:04:02 +0200

    Fix "no block given" error
    Under RedHat 6.7's ruby 1.8.7, it doesn't look like Pathname().each_filename
    likes being called without a block all that much.

--- lib/facter/tinc_keys.rb
+++ lib/facter/tinc_keys.rb
@@ -2,7 +2,7 @@ require 'pathname'
 
 nets = {}
 Dir.glob('/etc/tinc/*').each do |net|
-  netname = Pathname(net).each_filename.to_a[-1]
+  netname = Pathname(net).basename
   if File.exists?("/etc/tinc/#{netname}/rsa_key.pub")
     key = IO.read("/etc/tinc/#{netname}/rsa_key.pub")
     nets[netname] = key
